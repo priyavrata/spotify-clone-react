b1e8a3e63264833b5d456f21413c8ede766e0159
priyavrata <bansalpriyavrata@gmail.com>
2021-03-13 22:08:47 +0300

    Generated Components for Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+// import HomeIcon from "@material-ui/core"
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            Hi I am a sidebar
+            <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+            <SidebarOption Icon={HomeIcon} title="Home"/>
+            <SidebarOption Icon={SearchIcon} title="Search"/>
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+            <br/>
+            <strong className="sidebar_title">PLAYLISTS</strong>
+            <hr/>
+            <SidebarOption title="Hiphop"/>
+            <SidebarOption title="Rock"/>
+            <SidebarOption title="RnB"/>
+
         </div>
     );
 }
