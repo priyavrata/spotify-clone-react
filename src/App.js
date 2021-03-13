b5d3ce02f40559d68ff65c0dbdcd26efeebd2dde
priyavrata <bansalpriyavrata@gmail.com>
2021-03-13 20:12:46 +0300

    Added a Data Layer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {getTokenFromUrl} from './spotify';
 import './App.css';
+import Player from './Player.js';
 import Login from './Login.js';
+import SpotifyWebApi from 'spotify-web-api-js';
+import {useDataLayerValue} from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
+
+
 function App() {
+  const [token,setToken] = useState(null);
+  const [{user},dispatch] = useDataLayerValue;
 
   useEffect(()=>{
-    const token=getTokenFromUrl();
-    console.log('I HAVE A TOKEN ', token);
+    const hash=getTokenFromUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      });
+
+      
+    }
+    console.log("user: ",user);
   },[]);
 
   return (
     <div className="app">
+      {
+        token? (
+          <Player/>
+        ):
+      
         <Login />
+      }
     </div>
   );
 }
