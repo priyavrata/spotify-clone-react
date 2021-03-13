0fa3267e510784e2d58e78667c02b7cb4b5a259a
priyavrata <bansalpriyavrata@gmail.com>
2021-03-13 13:20:59 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Login from './Login.js';
+function App() {
+  return (
+    <div className="app">
+        <Login />
+    </div>
+  );
+}
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,16 @@
+export const authEndpoint = 
+"https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000";
+
+const clientId="985ffe7a441b4f9dad135ab0c26b6c83";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl=`${authEndpoint}?client_id=${clientId}&{redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
