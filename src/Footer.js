6841d560c956bc8c088619ad5d96aa02663c8762
priyavrata <bansalpriyavrata@gmail.com>
2021-03-14 00:39:28 +0300

    Footer Components created

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,49 @@
 import "./Footer.css";
 import React from 'react';
+import { Grid, Slider } from "@material-ui/core";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 
 function Footer(props) {
     return (
         <div className="footer">
-            Hi I am Footer
+            <div className="footer_left">
+                <img
+                    className="footer_albumLogo"
+                    src="https://randomwordgenerator.com/img/picture-generator/54e6d6474e53ae14f1dc8460962e33791c3ad6e04e5074417c2e7dd19245c0_640.jpg"
+                    alt=""
+                />
+            </div>
+
+            <div className="footer_center">
+                <ShuffleIcon className="footer_green"/>
+                <SkipPreviousIcon className="footer_icon"/>
+                <PlayCircleOutlineIcon fontSize="large" className="footer_icon"/>
+                <SkipNextIcon className="footer_icon"/>
+                <SkipNextIcon className="footer_green"/>
+            </div>
+
+            <div className="footer_right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous slider"/>
+                    </Grid>
+                </Grid>
+
+            </div>
+
         </div>
     );
 }
